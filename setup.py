937d996b158711767371363ed30231c03ead8007
dporret <david.porret@cern.ch>
2020-09-10 15:21:49 +0200

    project setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='ble_compass',
+setup(name='blecompass',
       version='0.1',
       description='3D compensated compass with BLE interface',
-      url='https://github.com/davPo/ble_compas.git',
+      url='https://github.com/davPo/blecompas.git',
       author='Dpo',
   #    author_email='flyingcircus@example.com',
       license='GNU GPLv3',
